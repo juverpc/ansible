8fb034fa6fa34d0a810ba94d149f0b3f451ac995
Juver <juverpc@gmail.com>
2021-10-06 13:09:36 -0500

    testing1

--- replace.py
+++ replace.py
@@ -4,11 +4,12 @@ import os
 
 path_yaml_playbook = "./playbook.yml"
 
-def read_playbook():
+def replace_playbook_values():
     read = yaml.safe_load(open(path_yaml_playbook ))
     read[0]['tasks'][0]['win_user']['name'] = 'Test'
     read[0]['tasks'][0]['win_user']['password'] = os.environ['VM_PASSWORD']
-    
+    result = yaml.dump(read)
+    print (result)
 
 if __name__ == "__main__":
-    read_playbook()
+    replace_playbook_values()
